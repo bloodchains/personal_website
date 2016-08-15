a149065b41e87f79a1fed84f975054922d97db66
epikkoder <ronaldsahagun87@gmail.com>
2016-08-15 02:10:09 -0700

    add files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Hind+Guntur|Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+        <meta charset="UTF-8">
+        <title>Ronald Sahagun</title>
+    </head>
+
+    <body>
+        <div id="mainContainer">
+            <?php include 'templates/header.html' ?>
+        </div>
+    </body>
+
+</html>
